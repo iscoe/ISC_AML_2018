e3b4ec49db3fc9bccbc92c4c8d2f881741d35d14
mjpekala <mike.pekala@jhuapl.edu>
2018-01-25 11:44:41 -0500

    added

---
+++ src/sample_attack.py
@@ -0,0 +1,10 @@
+#!/bin/bash python
+
+""" This script provides an example script that generates an attack for the 2018 ISC AML challenge.
+    
+    Participants are free (but not required) to use this as a template for developing their own attacks.
+"""
+
+
+if __name__ == "__main__":
+    print('TODO: coming soon!')
