f4b8fdfacb99a499d3f65ec62062d0651a000e3a
neilfendley <neilfendley@gmail.com>
2018-01-26 17:08:41 -0500

    adding basic framework utilities

--- src/docker_run.sh
+++ src/docker_run.sh
@@ -4,7 +4,6 @@ OUTPUT_DATA="/home/neilf/Fendley/adversarial/attackFmow/output"
 sudo nvidia-docker run \
 	-v ${INPUT_IMAGES}:/input_images \
 	-v ${OUTPUT_DATA}:/output \
-	-e USERID=$UID simple_defense \
-	./run_defense.sh \
+	--user www-data simple_defense ./run_defense.sh \
 	/input_images \
 	/output
