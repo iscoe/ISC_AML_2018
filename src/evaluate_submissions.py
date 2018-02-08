e493eb027d4f96a0d095d515f0018b801f8b7714
mjpekala <mike.pekala@jhuapl.edu>
2018-02-08 09:47:53 -0500

    ** updated how scores are tallied.  still under construction...

--- src/evaluate_submissions.py
+++ src/evaluate_submissions.py
@@ -33,14 +33,15 @@ import pdb
 from functools import partial
 from PIL import Image
 
-
 import numpy as np
+import pandas as pd
 
 
 
 N_CLASSES = 70  # TODO: fix this!
-ESTIMATES_FILE = 'estimates.csv'
-
+ESTIMATES_FILE = 'labels.csv'
+ATTACK_TAG = "attack"
+DEFENSE_TAG = "defense"
 
 
 # XXX: may update to use python logging package later
@@ -81,6 +82,7 @@ def _get_submission(team_dir, submission_type='attack'):
         submissions.extend(glob.glob(os.path.join(team_dir, pattern)))
 
     if len(submissions) == 0:
+        _info('No submission type "%s" in directory "%s"' % (submission_type, team_dir))
         return None
 
     # LINUX-SPECIFIC !!
@@ -121,25 +123,25 @@ def prepare_ae(ae_directory, tgt_directory, ref_directory, f_constraint):
 
     """
     if not os.path.exists(ae_directory):
-        _error('AE directory not found!')
+        _warning('AE directory "%s" not found - did attack not address this epsilon?' % ae_directory)
         return
   
-    cnt = 0
-    for cnt, filename in enumerate(_image_files(ref_directory)):
+    for filename in _image_files(ae_directory):
         path, img_name = os.path.split(filename)
-        x_ref = np.array(Image.open(filename), dtype=np.uint8)
+        x_ae = np.array(Image.open(filename), dtype=np.uint8)
 
-        # load the corresponding image from source dir
-        x_ae = np.array(Image.open(os.path.join(ae_directory, img_name)))
+        # load the corresponding image from reference directory
+        ref_file = os.path.join(ref_directory, img_name)
+        if not os.path.exists(ref_file):
+            _warning('no such reference file "%s"; ignoring this image' % ref_file)
+            continue
+        x_ref = np.array(Image.open(ref_file))
 
         # create the image that will actually be evaluated
         x_eval = f_constraint(x_ae, x_ref).astype(np.uint8)
 
         out_file = os.path.join(tgt_directory, img_name)
         Image.fromarray(x_eval, mode='RGB').save(out_file)
-
-    if cnt == 0:
-        _error('no reference files were found!  Is the setup correct?')
 
 
  
@@ -153,8 +155,7 @@ def _are_images_equivalent_p(dir_a, dir_b):
         img_b = Image.open(os.path.join(dir_b, fn))
         if np.any(np.array(img_a) != np.array(img_b)):
             return False
-        else:
-            return True
+    return True
 
 
 #-------------------------------------------------------------------------------
@@ -162,7 +163,13 @@ def _are_images_equivalent_p(dir_a, dir_b):
 #-------------------------------------------------------------------------------
 
 def load_estimates(csv_file_name):
-    """ Reads class estimates from a .csv file.
+    """ Reads class estimates (or truth) from a .csv file.
+
+        File format is assumed to be:
+
+        image_filename_1.png, y_1, [y_2, y_3, ...]
+        image_filename_2.png, y_1, [y_2, y_3, ...]
+        ...
     """
     file_names = []
     estimates = []
@@ -198,80 +205,144 @@ def _random_guessing_defense(input_dir, output_dir):
 #  Evauation (attack vs defense)
 #-------------------------------------------------------------------------------
 
-def run_attack_vs_defense(attack_zip, defense_zip, ref_dir, epsilon_values):
+def run_one_attack_vs_one_defense(attacker_id, attack_zip, defender_id, defense_zip, ref_dir, epsilon_values):
     """ Runs a single attack against a single defense.
 
     Produces a class estimate for every image/epsilon_value pair.
     """
-    # Create temporary files (will be deleted when objects go out of scope)
+
+    # ground truth
+    test_files, y_true = load_estimates(os.path.join(ref_dir, ESTIMATES_FILE))
+    n_test = len(test_files)
+    assert(y_true.size == n_test)  # truth file should have only one label
+
+    # Extract attack submission (only need to do this once - contains all epsilon) 
     raw_dir = tempfile.TemporaryDirectory()          # we unzip attacker's images here
-    def_in_dir = tempfile.TemporaryDirectory()       # images ready for defense live here
-    def_out_dir = tempfile.TemporaryDirectory()      # output from defense goes here
+    with ZipFile(attack_zip, 'r') as zf:
+        zf.extractall(path=raw_dir.name)
 
-    result = {}
-
+    results_attacker = []
+    results_defender = []
+    
     for epsilon in epsilon_values:
-        f_con = partial(enforce_ell_infty_constraint, epsilon=epsilon)
+        f_con = partial(enforce_ell_infty_constraint, epsilon=epsilon) # enforces constraint
+        def_in_dir = tempfile.TemporaryDirectory()       # images ready for defense live here
+        def_out_dir = tempfile.TemporaryDirectory()      # output from defense goes here
 
-        # unzip and prepare the attack images
-        with ZipFile(attack_zip, 'r') as zf:
-            zf.extractall(path=raw_dir.name)
-            input_dir = os.path.join(raw_dir.name, str(epsilon))
-            prepare_ae(input_dir, def_in_dir.name, ref_dir, f_con)
-            if not _are_images_equivalent_p(input_dir, def_in_dir.name):
-                _warning('input images did not satisfy constraints!!  They have been clipped accordingly.')
+        #----------------------------------------
+        # prepare the attack images for this value of epsilon
+        #----------------------------------------
+        input_dir = os.path.join(raw_dir.name, str(epsilon))
+        prepare_ae(input_dir, def_in_dir.name, ref_dir, f_con)
+        if not _are_images_equivalent_p(input_dir, def_in_dir.name):
+            pdb.set_trace() # TEMP
+            _warning('input images did not satisfy constraints!!  They have been clipped accordingly.')
+        attack_files = [os.path.basename(x) for x in _image_files(def_in_dir.name)]  # list of files created by attacker
 
+        #----------------------------------------
         # run defense on these images
+        #----------------------------------------
         # TODO: nvidia-docker run goes here!
-        pdb.set_trace() # TEMP
         _random_guessing_defense(def_in_dir.name, def_out_dir.name)
-        file_names, Y_hat = load_estimates(os.path.join(def_out_dir.name, ESTIMATES_FILE))
+        defense_files, Y_hat = load_estimates(os.path.join(def_out_dir.name, ESTIMATES_FILE))
 
-        # store result
-        result[epsilon] = (file_names, Y_hat)
+        #----------------------------------------
+        # evaluate performance
+        #----------------------------------------
+        attacker_score = np.zeros((n_test,))
+        defender_score = np.zeros((n_test,))
 
-    return result
+        for ii in range(len(test_files)):
+            fn, y_i= test_files[ii], y_true[ii]
+
+            # Special cases: attacker/defender does not produce/evaluate an example
+            #                Do not penalize the other party in this case (use NaN)
+            if fn not in attack_files:
+                attacker_score[ii] = 0
+                defender_score[ii] = np.nan
+                continue
+            elif fn not in defense_files:
+                attacker_score[ii] = np.nan
+                defender_score[ii] = 0
+                continue
+
+            # top-1 accuracy
+            idx = defense_files.index(fn)
+            y_hat_i = Y_hat[idx,0]
+            if y_hat_i == y_i:
+                attacker_score[ii] = 0
+                defender_score[ii] = 1
+            else:
+                attacker_score[ii] = 1
+                defender_score[ii] = 0
+
+        results_attacker.append((ATTACK_TAG, attacker_id, defender_id, epsilon) + tuple(attacker_score))
+        results_defender.append((DEFENSE_TAG, attacker_id, defender_id, epsilon) + tuple(defender_score))
+
+    return results_attacker, results_defender
+
+
+
+def run_attacks_vs_defenses(submission_dir, truth_dir, epsilon_values):
+    """ Runs each attack vs each defense.
+    """
+    all_attack = []
+    all_defense = []
+
+    for attacker_id in _all_team_names(submission_dir):
+        #----------------------------------------
+        # Get the attack submission
+        #----------------------------------------
+        _info('processing attacker: "%s"' % attacker_id)
+        attack_zip = _get_submission(os.path.join(submission_dir, attacker_id), 'attack')
+        if attack_zip is None:
+            continue # no attack from this team
+
+        for defender_id in _all_team_names(submission_dir):
+            #----------------------------------------
+            # get the defense submission
+            #----------------------------------------
+            if defender_id == attacker_id:  # do not play same team's attack vs defense
+                continue
+
+            _info('processing : "%s" vs "%s"' % (attacker_id, defender_id))
+            defense_zip = _get_submission(os.path.join(submission_dir, defender_id), 'defense')
+            if defense_zip is None:
+                continue # no defense submission from this team
+
+            #----------------------------------------
+            # run attack vs defense and store result
+            #----------------------------------------
+            #try:
+            attack_result, defense_result = run_one_attack_vs_one_defense(attacker_id, attack_zip, defender_id, defense_zip, truth_dir, epsilon_values)
+            all_attack.append(attack_result)
+            all_defense.append(defense_result)
+            #except Exception as ex:
+            #    _warning('%s vs %s failed! %s' % (attacker_id, defender_id, str(ex)))
+
+    return pd.DataFrame(all_attack), pd.DataFrame(all_defense)
+
+
+
+def compute_metrics(all_attacks, all_defenses):
+    # TODO: save out some .csv files
+    pass
 
 
 #-------------------------------------------------------------------------------
 #-------------------------------------------------------------------------------
 
 if __name__ == "__main__":
-    y_true = 'TODO'  # TODO: implement this.
-    submission_dir = '../misc/simulated_environment/submissions'  # TODO: fix this
-    truth_dir = '../misc/simulated_environment/truth'
-    epsilon_values_to_run = [10,]
+    submission_dir = sys.argv[1]
+    truth_dir = sys.argv[2]
+    epsilon_values_to_run = [int(x) for x in sys.argv[3:]]
 
+    if not os.path.isdir(submission_dir):
+        raise RuntimeError('Invalid submission directory: "%s"' % submission_dir)
+    
+    if not os.path.isdir(truth_dir):
+        raise RuntimeError('Invalid truth directory: "%s"' % truth_dir)
 
-    #--------------------------------------------------
-    # Run all attacks vs all defenses
-    #--------------------------------------------------
-    all_results = {}  
-
-    for attacker in _all_team_names(submission_dir):
-        all_results[attacker] = {}
-
-        attack_zip = _get_submission(os.path.join(submission_dir, attacker), 'attack')
-        if attack_zip is None:
-            continue # no attack from this team
-        else:
-            _info('processing attack file %s' % attack_zip)
-
-        for defender in _all_team_names(submission_dir):
-            # we do not play the same team's attack vs defense
-            if defender == attacker:
-                continue
-
-            defense_zip = _get_submission(os.path.join(submission_dir, defender), 'defense')
-            if defense_zip is None:
-                continue # no defense from this team
-            else:
-                _info('processing defense file %s' % defense_zip)
-
-            # try out an interaction between attack and defense
-
-            result = run_attack_vs_defense(attack_zip, defense_zip, truth_dir, epsilon_values_to_run)
-            all_results[attacker][defender] = result
-
-    print(all_results) # TEMP
-    compute_metrics(all_results, y_true)
+    attacks, defenses = run_attacks_vs_defenses(submission_dir, truth_dir, epsilon_values_to_run)
+    
+    compute_metrics(attacks, defenses)
