fc5445f9b6fb6504cee02978ff291f8cc973a8c3
neilfendley <neilfendley@gmail.com>
2018-02-09 14:28:56 -0500

    updating documentation

--- src/README.md
+++ src/README.md
@@ -1,6 +1,9 @@
 # ISC_AML_2018
 
-### The challenge team provides a few basic adversarial attacks and defenses. These are contained in the respective attacks and defenses folders.
-### In order to run the attacks against the defenses, call evaluate_submissions.py.
+## Challenge Framework
+The challenge team provides a few basic adversarial attacks and defenses. These are contained in the respective attacks and defenses folders.  
+In order to run the provided attacks against defenses use the run_submissions function in docker_run.py
+
+
 
 
--- src/docker_run.py
+++ src/docker_run.py
@@ -3,29 +3,25 @@ import json
 import os
 import subprocess
 
-
-def main():
-    ### Use this to run on the default submissions 
-    base_dir = '/home/neilf/Fendley/adversarial/ISC_AML_2018'
-    output_dir = os.path.join(base_dir, 'output/')
-    sample_defense_dir = os.path.join(base_dir,'src/defenses/sample_defense_submission/')
-    sample_offense_dir = os.path.join(base_dir, 'image_sets/test_images')
-    
-    defense_folders = []
-    offense_folders = []
-    offense_folders.append(sample_offense_dir) 
-    defense_folders.append(sample_defense_dir)
-    
-    run_submissions(defense_folders,offense_folders,output_dir)
-
-
+"""
+This function will run the given defenses against provided adversarial attacked image sets. It will write out predictions
+of every defense against each adversarially attacked image set to the specified folder
+[  Params  ]:
+  defense_folders: a list of paths to the defense submission folders. Must contain metadata.json
+  offense_folders: a list of paths to the adversarially attacked image sets provided by offenses
+  out_dir: folder to write predictions out, must be owned by www-data:www-data as user:group 
+"""
 def run_submissions(defense_folders,offense_folders,output_dir):
  
     for defense in defense_folders:
       for offense in offense_folders:
         run_defense(defense, offense, output_dir)
 
-    
+"""
+Helper function to run the defenses
+[  Params  ]:
+  SEE ABOVE
+"""
 def run_defense(submission_dir, offense_dir, output_dir):
     #Load metadata from their submission
     metadata = json.load(open(os.path.join(submission_dir,'metadata.json')))
@@ -42,6 +38,19 @@ def run_defense(submission_dir, offense_dir, output_dir):
     subprocess.call(cmd)
 
 
+def main():
+    ### Use this to run on the default submissions 
+    base_dir = '/home/neilf/Fendley/adversarial/ISC_AML_2018'
+    output_dir = os.path.join(base_dir, 'output/')
+    sample_defense_dir = os.path.join(base_dir,'src/defenses/sample_defense_submission/')
+    sample_offense_dir = os.path.join(base_dir, 'image_sets/test_images')
+    
+    defense_folders = []
+    offense_folders = []
+    offense_folders.append(sample_offense_dir) 
+    defense_folders.append(sample_defense_dir)
+    
+    run_submissions(defense_folders,offense_folders,output_dir)
 
 if __name__ == '__main__':
     main()
